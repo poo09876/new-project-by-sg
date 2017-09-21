158afe7a0f87e19fd47a5208777b2eeadb389ad1
sunshuo <826674260@qq.com>
2017-09-21 09:07:24 +0800

    sunshuo

---
+++ demo3/src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>例子目录:</h2>
+    <ol>
+      <li>父->子 组件通讯：props <a href="/props">点击进入</a></li>
+      <li>子 >父-组件通讯：Emit <a href="/emit">点击进入</a></li>
+      <li>普通组件通讯：Global Bus <a href="/bus">点击进入</a></li>
+      <li></li>
+      <li></li>
+    </ol>
+  </div>
+</template>
+
---
+++ demo3/src/routers.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+import Index    from '@/pages/Index'
+import ParentProps  from '@/pages/1-props/Parent.vue'
+import ParentEmit  from '@/pages/2-emit/Parent.vue'
+import BusDemo  from '@/pages/3-bus/Demo.vue'
+export default new Router({
+  mode:"history",
+  routes: [
+    { path: '/',          name: 'Index',          component: Index        },
+    { path: '/props',     name: 'ParentProps',    component: ParentProps  },
+    { path: '/emit',      name: 'ParentEmit',     component: ParentEmit   },
+    { path: '/bus',       name: 'BusDemo',        component: BusDemo      },
+  ]
+})
